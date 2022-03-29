cb8157ac3b16ddb376d4ff8f53a14257a6f23c52
Ksenia Busquet <ksenia.busquet@gmail.com>
2022-03-29 06:03:10 -0300

    All iterations complete

--- src/clue.js
+++ src/clue.js
@@ -5,63 +5,63 @@
 const suspectsArray = [
 
 {
-firstName: Jacob,
-lastName: Green,
-occupation: Entrepreneur,
+firstName: "Jacob",
+lastName: "Green",
+occupation: "Entrepreneur",
 age: 45,
 description: "He has a lot of connections",
 image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg", 
-color: green
+color: "green"
 },
 
 {
-firstName: Doctor,
-lastName: Orchid,
-occupation: Scientist,
+firstName: "Doctor",
+lastName: "Orchid",
+occupation: "Scientist",
 age: 26,
 description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
 image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-color: white
+color: "white"
 },
 
 {
-firstName: Victor,
-lastName: Plum,
-occupation: Designer,
+firstName: "Victor",
+lastName: "Plum",
+occupation: "Designer",
 age: 22,
 description: "Billionaire video game designer",
 image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
-color: purple
+color: "purple"
 },
 
 {
-firstName: Kasandra,
-lastName: Scarlet,
-occupation: Actor,
+firstName: "Kasandra",
+lastName: "Scarlet",
+occupation: "Actor",
 age: 31,
 description: "She is an A-list movie star with a dark past",
 image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
-color: red
+color: "red"
 },
 
 {
-firstName: Eleanor,
-lastName: Peacock,
-occupation: Socialité,
+firstName: "Eleanor",
+lastName: "Peacock",
+occupation: "Socialité",
 age: 36,
 description: "She is from a wealthy family and uses her status and money to earn popularity",
 image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
-color: blue
+color: "blue"
 },
 
 {
-firstName: Jack,
-lastName: Mustard,
+firstName: "Jack",
+lastName: "Mustard",
 occupation: "Retired Football player",
 age: 62,
 description: "He is a former football player who tries to get by on his former glory",
 image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
-color: yellow
+color: "yellow"
 }
 
 ];
@@ -71,57 +71,77 @@ color: yellow
 const roomsArray = [
 
   {name: "Dining Room"},
-  {name: Conservatory},
-  {name: Kitchen},
-  {name: Study},
-  {name: Library},
+  {name: "Conservatory"},
+  {name: "Kitchen"},
+  {name: "Study"},
+  {name: "Library"},
   {name: "Billiard Room"},
-  {name: Lounge},
-  {name: Ballroom},
-  {name: Hall},
-  {name: Spa},
+  {name: "Lounge"},
+  {name: "Ballroom"},
+  {name: "Hall"},
+  {name: "Spa"},
   {name: "Living Room"},
-  {name: Observatory},
-  {name: Theater},
+  {name: "Observatory"},
+  {name: "Theater"},
   {name: "Guest House"},
-  {name: Patio}
+  {name: "Patio"}
 
 ];
 
 // Weapons Array
 
 const weaponsArray = [
-  {name: rope,
+  {name: "rope",
   weight: 10},
-  {name: knife,
+  {name: "knife",
   weight: 8},
-  {name: candlestick,
+  {name: "candlestick",
    weight: 2},
-  {name: dumbbell,
+  {name: "dumbbell",
   weight: 30},
-  {name: poison,
+  {name: "poison",
   weight: 2},
-  {name: axe,
+  {name: "axe",
   weight: 15},
-  {name: bat,
+  {name: "bat",
    weight: 13},
-  {name: trophy,
+  {name: "trophy",
   weight: 25},
-  {name: pistol,
+  {name: "pistol",
   weight: 20}
 ];
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arr) {
+  return arr[Math.floor(Math.random()*arr.length)];
+}
 
-function pickMystery() {}
+function pickMystery() {
+  let randomObject = {};
+  randomObject.suspect = selectRandom(suspectsArray);
+  randomObject.weapon = selectRandom(weaponsArray);
+  randomObject.room = selectRandom(roomsArray);
+  return randomObject;
+
+}
 
 
 // ITERATION 3
+/*Object structure
 
-function revealMystery() {}
+randomObject {
+  suspect: {},
+  weapon: {}
+  room: {}
+}
+*/
+
+function revealMystery(envelope) {
+return (`${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`);
+
+}
 
 
 
